17c20bfdc58077a1b3d0c8c2d48b8f16adcefb68
WEN Hao <wenh06@gmail.com>
2021-03-30 19:01:14 +0800

    add entry file from official repo

---
+++ entry_2021.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import os
+import sys
+
+import wfdb
+from utils.misc import save_dict
+
+"""
+Written by:  Xingyao Wang, Chengyu Liu
+             School of Instrument Science and Engineering
+             Southeast University, China
+             chengyu@seu.edu.cn
+
+Save answers to '.json' files, the format is as {‘predict_endpoints’: [[s0, e0], [s1, e1], …, [sm-1, em-2]]}.
+"""
+
+
+def challenge_entry(sample_path):
+    """
+    This is a baseline method.
+    """
+
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+    DATA_PATH = sys.argv[1]
+    RESULT_PATH = sys.argv[2]
+    if not os.path.exists(RESULT_PATH):
+        os.makedirs(RESULT_PATH)
+        
+    test_set = open(os.path.join(DATA_PATH, 'RECORDS'), 'r').read().splitlines()
+    for i, sample in enumerate(test_set):
+        print(sample)
+        sample_path = os.path.join(DATA_PATH, sample)
+        pred_dict = challenge_entry(sample_path)
+
+        save_dict(os.path.join(RESULT_PATH, sample+'.json'), pred_dict)
+
