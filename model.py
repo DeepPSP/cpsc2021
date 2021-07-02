28a4803cd237f04d387e281e0b3dc3f7ad3db6f7
WEN Hao <wenh06@gmail.com>
2021-07-02 11:11:05 +0800

    list possible model candidates

--- model.py
+++ model.py
@@ -1,2 +1,9 @@
 """
+
+Possible Solutions
+------------------
+1. segmentation (AF, non-AF) -> postprocess (merge too-close intervals, etc) -> onsets & offsets
+2. sequence labelling (AF, non-AF) -> postprocess (merge too-close intervals, etc) -> onsets & offsets
+3. per-beat (R peak detection first) classification (CNN, etc. + RR LSTM) -> postprocess (merge too-close intervals, etc) -> onsets & offsets
+4. object detection (? onsets and offsets)
 """
