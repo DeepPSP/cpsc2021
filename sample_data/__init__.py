9c977e0ba8233570994685f54ac4a2df04e1dc29
WEN Hao <wenh06@gmail.com>
2021-10-18 17:13:10 +0800

    add __init__ file for sample data

---
+++ sample_data/__init__.py
@@ -0,0 +1,22 @@
+"""
+"""
+
+import os, zipfile, glob
+
+
+_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+_WORK_DIR = os.path.join(_BASE_DIR, "working_dir")
+_SAMPLE_DATA_DIR = os.path.join(_WORK_DIR, "sample_data")
+
+
+__all__ = ["extract_sample_data_if_needed",]
+
+
+def extract_sample_data_if_needed():
+    """
+    """
+    if os.path.exists(_SAMPLE_DATA_DIR) and len(glob.glob(os.path.join(_SAMPLE_DATA_DIR, "*.dat"))) > 0:
+        return
+    os.makedirs(_SAMPLE_DATA_DIR, exist_ok=True)
+    zf = zipfile.ZipFile(os.path.join(_SAMPLE_DATA_DIR, "sample_data.zip"))
+    zf.extractall(_WORK_DIR)
